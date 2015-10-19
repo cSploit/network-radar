05783676a848d3a27c56ce864565d6e9a28a8257
tux-mind <massimo.dragano@gmail.com>
2015-10-19 14:06:08 +0200

    define GNU_SOURCE once

--- ifinfo.c
+++ ifinfo.c
@@ -14,8 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with cSploit.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 
 #include <string.h>
 #include <errno.h>
